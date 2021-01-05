b13cf99a8635285a2d4349ee009bc3fb5b11135a
Stepanov Nikita <stepanovnv95@gmail.com>
2021-01-05 16:15:56 +0400

    First working version with aligned_storage

--- Vector.h
+++ Vector.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <type_traits>
 
 
 namespace study {
@@ -12,24 +13,31 @@ class Vector
 {
 public:
     using value_type = T;
+private:
+    using StorageType = std::aligned_storage_t<sizeof(value_type), std::alignment_of_v<value_type>>;
+public:
 
     Vector()
         : capacity_(0),
           size_(0)
     {}
 
-    explicit Vector(size_t capacity)
-        : capacity_(capacity),
-          size_(capacity),
-          data_(std::unique_ptr<T[]>(new T[capacity]))
-    {}
+    explicit Vector(size_t size)
+        : capacity_(size),
+          size_(size),
+          data_(makeStorage(size))
+    {
+        for (auto it = data_.get(); it != data_.get() + size; ++it) {
+            new (std::launder(reinterpret_cast<value_type*>(it))) value_type;
+        }
+    }
 
     Vector(const Vector &other)
         : capacity_(other.capacity_),
           size_(other.size_),
-          data_(std::unique_ptr<T[]>(new T[capacity_]))
+          data_(makeStorage(capacity_))
     {
-        std::copy(other.data_.get(), other.data_.get() + size_, data_.get());
+        copyValues(other.data_.get(), data_.get(), size_);
     }
 
     Vector& operator=(const Vector &other)
@@ -38,46 +46,48 @@ public:
         return *this;
     }
 
-    Vector(Vector&& other)
+    Vector(Vector&& other) noexcept
     {
         swap(other);
     }
 
-    Vector& operator=(Vector&& other)
+    Vector& operator=(Vector&& other) noexcept
     {
         swap(other);
         return *this;
     }
 
+    [[nodiscard]]
     size_t capacity() const
     {
         return capacity_;
     }
 
+    [[nodiscard]]
     size_t size() const
     {
         return size_;
     }
 
-    const T& operator[](size_t i) const
+    const value_type& operator[](size_t i) const
     {
-        return data_[i];
+        return *std::launder(reinterpret_cast<value_type*>(data_.get() + i));
     }
 
-    T& operator[](size_t i)
+    value_type& operator[](size_t i)
     {
-        return data_[i];
+        return *std::launder(reinterpret_cast<value_type*>(data_.get() + i));
     }
 
-    void push_back(const T &obj)
+    void push_back(value_type&& obj)
     {
         if (size_ == capacity_) {
             reallocate();
         }
-        pushToEnd(obj);
+        pushToEnd(std::forward<value_type>(obj));
     }
 
-    void swap(Vector &other)
+    void swap(Vector &other) noexcept
     {
         std::swap(capacity_, other.capacity_);
         std::swap(size_, other.size_);
@@ -86,6 +96,7 @@ public:
 
     void resize(size_t count)
     {
+        // TODO
         if (count < capacity_) {
             reallocate(count);
         }
@@ -104,7 +115,7 @@ public:
 
     private:
         friend class Vector;
-        Iterator(T *ptr)
+        explicit Iterator(StorageType* ptr)
             : ptr_(ptr)
         {}
 
@@ -125,41 +136,57 @@ public:
             return *this;
         }
 
-        reference operator*()
-        {
-            return *ptr_;
-        }
-
         pointer operator->()
         {
-            return ptr_;
+            return std::launder(reinterpret_cast<pointer>(ptr_));
+        }
+
+        reference operator*()
+        {
+            return *operator->();
         }
 
     private:
-        VT* ptr_;
+        StorageType* ptr_;
     };
 
-    Iterator<T> begin()
+    Iterator<value_type> begin()
     {
-        return Iterator<T>(data_.get());
+        return Iterator<value_type>(data_.get());
     }
 
-    Iterator<T> end()
+    Iterator<value_type> end()
     {
-        return Iterator<T>(data_.get() + size_);
+        return Iterator<value_type>(data_.get() + size_);
     }
 
-    Iterator<const T> cbegin() const
+    Iterator<const value_type> cbegin() const
     {
-        return Iterator<const T>(data_.get());
+        return Iterator<const value_type>(data_.get());
     }
 
-    Iterator<const T> cend() const
+    Iterator<const value_type> cend() const
     {
-        return Iterator<const T>(data_.get() + size_);
+        return Iterator<const value_type>(data_.get() + size_);
     }
 
 private:
+    std::unique_ptr<StorageType[]> makeStorage(size_t size) const
+    {
+        return std::unique_ptr<StorageType[]>(new StorageType[size]);
+    }
+
+    void copyValues(StorageType* source, StorageType* target, size_t size)
+    {
+        StorageType* otherStorage = source;
+        StorageType* storage = target;
+        for (; otherStorage != source + size; ++otherStorage, ++storage) {
+            value_type* otherValue = std::launder(reinterpret_cast<value_type*>(otherStorage));
+            value_type* value = std::launder(reinterpret_cast<value_type*>(storage));
+            new (value) value_type(*otherValue);
+        }
+    }
+
     void reallocate(size_t count=0)
     {
         size_t newCapacity;
@@ -168,21 +195,21 @@ private:
         } else {
             newCapacity = count;
         }
-        auto newData = std::unique_ptr<T[]>(new T[newCapacity]);
-        std::copy(data_.get(), data_.get() + size_, newData.get());
+        auto newData = makeStorage(newCapacity);
+        copyValues(data_.get(), newData.get(), size_);
         data_.swap(newData);
         capacity_ = newCapacity;
     }
 
-    void pushToEnd(const T &obj)
+    void pushToEnd(value_type&& obj)
     {
-        data_[size_] = obj;
+        new (std::launder(reinterpret_cast<value_type*>(data_.get() + size_))) value_type(std::forward<value_type>(obj));
         ++size_;
     }
 
     size_t capacity_;
     size_t size_;
-    std::unique_ptr<T[]> data_;
+    std::unique_ptr<StorageType[]> data_;
 };
 
 }
